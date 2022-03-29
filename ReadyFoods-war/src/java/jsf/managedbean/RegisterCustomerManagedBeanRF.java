f0120218f1e4704f6d80307d9e347a9aa48e3dfe
Cassia <e0564059@u.nus.edu>
2022-03-29 15:36:13 +0800

    Registration feature

--- ReadyFoods-war/src/java/jsf/managedbean/RegisterCustomerManagedBeanRF.java
+++ ReadyFoods-war/src/java/jsf/managedbean/RegisterCustomerManagedBeanRF.java
@@ -3,31 +3,44 @@ package jsf.managedbean;
 import ejb.session.stateless.CustomerSessionBeanLocal;
 import entity.Customer;
 import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
+import javax.faces.view.ViewScoped;
+import util.enumeration.ActivityLevel;
+import util.enumeration.DietType;
+import util.enumeration.Gender;
 import util.exception.CustomerEmailExistsException;
 import util.exception.CustomerNotFoundException;
 import util.exception.InputDataValidationException;
 import util.exception.UnknownPersistenceException;
 
 @Named(value = "registerCustomerManagedBeanRF")
-@RequestScoped
+@ViewScoped
 public class RegisterCustomerManagedBeanRF implements Serializable {
 
     @EJB
     private CustomerSessionBeanLocal customerSessionBeanLocal;
 
     private Customer newCustomer;
+    private Gender[] genders;
+    private ActivityLevel[] activityLevels;
+    private DietType[] dietTypes;
     
     public RegisterCustomerManagedBeanRF() 
     {
         newCustomer = new Customer();
     }
     
+    @PostConstruct 
+    public void postConstruct() {
+
+    }
     public void registerNewCustomer(ActionEvent event)
     {        
                   
@@ -57,6 +70,48 @@ public class RegisterCustomerManagedBeanRF implements Serializable {
      */
     public void setNewCustomer(Customer newCustomer) {
         this.newCustomer = newCustomer;
+    }
+
+    /**
+     * @return the genders
+     */
+    public Gender[] getGenders() {
+        return Gender.values();
+    }
+
+    /**
+     * @param genders the genders to set
+     */
+    public void setGenders(Gender[] genders) {
+        this.genders = genders;
+    }
+
+    /**
+     * @return the activityLevels
+     */
+    public ActivityLevel[] getActivityLevels() {
+        return ActivityLevel.values();
+    }
+
+    /**
+     * @param activityLevels the activityLevels to set
+     */
+    public void setActivityLevels(ActivityLevel[] activityLevels) {
+        this.activityLevels = activityLevels;
+    }
+
+    /**
+     * @return the dietTypes
+     */
+    public DietType[] getDietTypes() {
+        return DietType.values();
+    }
+
+    /**
+     * @param dietTypes the dietTypes to set
+     */
+    public void setDietTypes(DietType[] dietTypes) {
+        this.dietTypes = dietTypes;
     }
     
 }
