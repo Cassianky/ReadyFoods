752ff9e8d66263ddbc32e65bca07c48bf00cb500
e056 <e0560031@u.nus.edu>
2022-04-03 22:16:23 +0800

    datainit, sesh beans for recipe ingredient and inredientSpec

--- ReadyFoods-ejb/src/java/ejb/session/singleton/DataInitSessionBean.java
+++ ReadyFoods-ejb/src/java/ejb/session/singleton/DataInitSessionBean.java
@@ -4,15 +4,18 @@ import ejb.session.stateless.CategorySessionBeanLocal;
 import ejb.session.stateless.CustomerSessionBeanLocal;
 import ejb.session.stateless.IngredientSessionBean;
 import ejb.session.stateless.IngredientSessionBeanLocal;
+import ejb.session.stateless.IngredientSpecificaitonSessionBeanLocal;
 import ejb.session.stateless.RecipeSessionBeanLocal;
 import entity.Category;
 import entity.Customer;
 import entity.Ingredient;
-import entity.IngredientSpecifcation;
+import entity.IngredientSpecification;
 import entity.Recipe;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
@@ -30,6 +33,7 @@ import util.exception.CreateRecipeException;
 import util.exception.CustomerEmailExistsException;
 import util.exception.CustomerNotFoundException;
 import util.exception.IngredientExistsException;
+import util.exception.IngredientNotFoundException;
 import util.exception.InputDataValidationException;
 import util.exception.RecipeNotFoundException;
 import util.exception.RecipeTitleExistException;
@@ -40,6 +44,9 @@ import util.exception.UnknownPersistenceException;
 @Startup
 
 public class DataInitSessionBean {
+
+    @EJB
+    private IngredientSpecificaitonSessionBeanLocal ingredientSpecificationSessionBeanLocal;
 
     @EJB
     private CustomerSessionBeanLocal customerSessionBeanLocal;
@@ -68,29 +75,39 @@ public class DataInitSessionBean {
             Customer customer1 = new Customer("customer1", "customer1", "customer1", "99999999", "password", "customer1@gmail.com", "123 Street", 20, DietType.VEGAN, Gender.FEMALE, ActivityLevel.HIGH);
             customerSessionBeanLocal.createNewCustomer(customer1);
 
-            Ingredient ingredient1 = new Ingredient("Chicken", "Whole Chicken between 1kg to 1.5kg", IngredientUnit.Whole, new BigDecimal("10.00"),  50, 150);
-            Ingredient ingredient2 = new Ingredient("Salt", "Salt by grams", IngredientUnit.Gram, new BigDecimal("0.05"), 100*1000, 20*1000);
+            Ingredient ingredient1 = new Ingredient("Chicken", "Whole Chicken between 1kg to 1.5kg", IngredientUnit.Whole, new BigDecimal("10.00"), 50, 150);
+            Ingredient ingredient2 = new Ingredient("Salt", "Salt by grams", IngredientUnit.Gram, new BigDecimal("0.05"), 100 * 1000, 20 * 1000);
             Ingredient ingredient3 = new Ingredient("Scallion", "Scallion by bunch", IngredientUnit.Whole, new BigDecimal("0.50"), 50, 10);
-            Ingredient ingredient4 = new Ingredient("Sesame Oil", "Sesame Oil by 100 ml", IngredientUnit.Gram, new BigDecimal("0.80"),  50*1000, 10*1000);
+            Ingredient ingredient4 = new Ingredient("Sesame Oil", "Sesame Oil by 100 ml", IngredientUnit.Gram, new BigDecimal("0.80"), 50 * 1000, 10 * 1000);
             Ingredient ingredient5 = new Ingredient("Ginger", "Whole Ginger between 15-30g", IngredientUnit.Whole, new BigDecimal("0.50"), 50, 10);
             Ingredient ingredient6 = new Ingredient("Rice", "Rice 1kg per pack", IngredientUnit.Kilogram, new BigDecimal("2.50"), 200, 50);
 
-            ingredientSessionBeanLocal.createNewIngredient(ingredient1);
-            ingredientSessionBeanLocal.createNewIngredient(ingredient2);
-            ingredientSessionBeanLocal.createNewIngredient(ingredient3);
-            ingredientSessionBeanLocal.createNewIngredient(ingredient4);
-            ingredientSessionBeanLocal.createNewIngredient(ingredient5);
-            
-            
-            
-            List<IngredientSpecifcation> ingredientSpecications = new ArrayList<>();
-            ingredientSpecications.add(new IngredientSpecifcation(1, ingredient1));
-            ingredientSpecications.add(new IngredientSpecifcation(50, ingredient2));
-            ingredientSpecications.add(new IngredientSpecifcation(1, ingredient3));
-            ingredientSpecications.add(new IngredientSpecifcation(30, ingredient4));
-            ingredientSpecications.add(new IngredientSpecifcation(1, ingredient5));
-            ingredientSpecications.add(new IngredientSpecifcation(1, ingredient6));
-            
+            Long ingredient1Id = ingredientSessionBeanLocal.createNewIngredient(ingredient1);
+            Long ingredient2Id = ingredientSessionBeanLocal.createNewIngredient(ingredient2);
+            Long ingredient3Id = ingredientSessionBeanLocal.createNewIngredient(ingredient3);
+            Long ingredient4Id = ingredientSessionBeanLocal.createNewIngredient(ingredient4);
+            Long ingredient5Id = ingredientSessionBeanLocal.createNewIngredient(ingredient5);
+
+            IngredientSpecification ingredientSpecifcation1 = new IngredientSpecification(1);
+            IngredientSpecification ingredientSpecifcation2 = new IngredientSpecification(50);
+            IngredientSpecification ingredientSpecifcation3 = new IngredientSpecification(1);
+            IngredientSpecification ingredientSpecifcation4 = new IngredientSpecification(30);
+            IngredientSpecification ingredientSpecifcation5 = new IngredientSpecification(1);
+       
+
+            Long ingredientSpecification1Id = ingredientSpecificationSessionBeanLocal.createNewIngredientSpecification(ingredientSpecifcation1, 1L);
+            Long ingredientSpecification2Id = ingredientSpecificationSessionBeanLocal.createNewIngredientSpecification(ingredientSpecifcation2, ingredient2Id);
+            Long ingredientSpecification3Id = ingredientSpecificationSessionBeanLocal.createNewIngredientSpecification(ingredientSpecifcation3, ingredient3Id);
+            Long ingredientSpecification4Id = ingredientSpecificationSessionBeanLocal.createNewIngredientSpecification(ingredientSpecifcation4, ingredient4Id);
+            Long ingredientSpecification5Id = ingredientSpecificationSessionBeanLocal.createNewIngredientSpecification(ingredientSpecifcation5, ingredient5Id);
+            System.out.println("INGREDIENT SPECIFICATION: " + ingredientSpecification1Id);
+            List<Long> ingredientSpecicationsId = new ArrayList<>();
+            ingredientSpecicationsId.add(ingredientSpecification1Id);
+            ingredientSpecicationsId.add(ingredientSpecification2Id);
+            ingredientSpecicationsId.add(ingredientSpecification3Id);
+            ingredientSpecicationsId.add(ingredientSpecification4Id);
+            ingredientSpecicationsId.add(ingredientSpecification5Id);
+
             String recipeSteps = "Preparation" + "/n"
                     + "To clean the chicken, rub all over with a handful of kosher salt, getting rid of any loose skin. Rinse the chicken well inside and out. Pat dry with paper towels." + "/n"
                     + "Remove any excess fat from the chicken and set aside for later." + "/n"
@@ -110,9 +127,8 @@ public class DataInitSessionBean {
                     + "Enjoy!";
 
             Recipe recipe1 = new Recipe("Hainanese Chicken Rice", "Chicken King", 45, recipeSteps, 1248, 87, 76, 47, 9, "https://youtu.be/XPA3rn1XImY");
-            
-            recipe1.getIngredientSpecificationList().addAll(ingredientSpecications);
-            
+
+
             Category parentCategory1 = categorySessionBeanLocal.createNewCategory(new Category("Cuisine", "Cuisine"), null);
             Category childCategory11 = categorySessionBeanLocal.createNewCategory(new Category("Asian", "Asian"), parentCategory1.getCategoryId());
             Category childCategory12 = categorySessionBeanLocal.createNewCategory(new Category("Western", "Western"), parentCategory1.getCategoryId());
@@ -125,17 +141,19 @@ public class DataInitSessionBean {
             Category childCategory23 = categorySessionBeanLocal.createNewCategory(new Category("Lunch", "Lunch"), parentCategory2.getCategoryId());
             Category childCategory24 = categorySessionBeanLocal.createNewCategory(new Category("Tea", "Tea"), parentCategory2.getCategoryId());
             Category childCategory25 = categorySessionBeanLocal.createNewCategory(new Category("Dinner", "Dinner"), parentCategory2.getCategoryId());
-            
+
             List<Long> recipe1Categories = new ArrayList<>();
             recipe1Categories.add(childCategory11.getCategoryId());
             recipe1Categories.add(childCategory23.getCategoryId());
             recipe1Categories.add(childCategory25.getCategoryId());
-            
-            recipeSessionBeanLocal.createNewRecipe(recipe1, recipe1Categories);
 
-        } catch (InputDataValidationException | UnknownPersistenceException | CustomerEmailExistsException |CategoryNotFoundException 
-                | CreateCategoryException |CreateRecipeException | RecipeTitleExistException | IngredientExistsException ex) {
+            recipeSessionBeanLocal.createNewRecipe(recipe1, recipe1Categories, ingredientSpecicationsId);
+
+        } catch (InputDataValidationException | UnknownPersistenceException | CustomerEmailExistsException | CategoryNotFoundException
+                | CreateCategoryException | CreateRecipeException | RecipeTitleExistException | IngredientExistsException ex) {
             ex.printStackTrace();
+        } catch (IngredientNotFoundException ex) {
+            Logger.getLogger(DataInitSessionBean.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
