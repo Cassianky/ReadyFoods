03afd86818dad563b01fb25f7b0539cb9561c24f
angely <e0564603@u.nus.edu>
2022-04-14 13:52:56 +0800

    updates to ws

--- ReadyFoodsRws/src/java/ws/rest/EnquiryResource.java
+++ ReadyFoodsRws/src/java/ws/rest/EnquiryResource.java
@@ -20,11 +20,13 @@ import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+import util.exception.InvalidLoginCredentialException;
 
 /**
  * REST Web Service
@@ -33,11 +35,11 @@ import javax.ws.rs.core.Response.Status;
  */
 @Path("Enquiry")
 public class EnquiryResource {
-    
+
     StaffSessionBeanLocal staffSessionBeanLocal = lookupStaffSessionBeanLocal();
-    
+
     EnquirySessionBeanLocal enquirySessionBeanLocal = lookupEnquirySessionBeanLocal();
-    
+
     @Context
     private UriInfo context;
 
@@ -46,7 +48,7 @@ public class EnquiryResource {
      */
     public EnquiryResource() {
     }
-    
+
     @Path("retrieveAllEnquires")
     @GET
     @Consumes(MediaType.TEXT_PLAIN)
@@ -54,27 +56,54 @@ public class EnquiryResource {
     public Response retrieveAllEnquires(@QueryParam("username") String username,
             @QueryParam("password") String password) {
         try {
-            
+
             Staff staff = staffSessionBeanLocal.staffLogin(username, password);
-            
+
             System.out.println("********** EnquiryResource.retrieveAllEnquires(): Staff "
                     + staff.getUsername() + " login remotely via web service");
-            
+
             List<Enquiry> enquires = enquirySessionBeanLocal.retrieveAllEnquires();
-            
+
             for (Enquiry enquiry : enquires) {
                 enquiry.setCustomer(null);
             }
-            
+
             GenericEntity<List<Enquiry>> genericEntity = new GenericEntity<List<Enquiry>>(enquires) {
             };
-            
+
             return Response.status(Status.OK).entity(genericEntity).build();
+        } catch (InvalidLoginCredentialException ex) {
+            return Response.status(Status.UNAUTHORIZED).entity(ex.getMessage()).build();
         } catch (Exception ex) {
             return Response.status(Status.INTERNAL_SERVER_ERROR).entity(ex.getMessage()).build();
         }
     }
-    
+
+    @Path("retrieveEnquiry/{enquiryId}")
+    @GET
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response retrieveEnquiry(
+            @QueryParam("username") String username,
+            @QueryParam("password") String password,
+            @PathParam("enquiryId") Long enquiryId) {
+        try {
+
+            Staff staff = staffSessionBeanLocal.staffLogin(username, password);
+
+            System.out.println("********** EnquiryResource.retrieveEnquiry(): Staff "
+                    + staff.getUsername() + " login remotely via web service");
+
+            Enquiry enquiry = enquirySessionBeanLocal.retrieveEnquiryByEnquiryId(enquiryId);
+
+            enquiry.setCustomer(null);
+
+            return Response.status(Status.OK).entity(enquiry).build();
+        } catch (Exception ex) {
+            return Response.status(Status.INTERNAL_SERVER_ERROR).entity(ex.getMessage()).build();
+        }
+    }
+
     private EnquirySessionBeanLocal lookupEnquirySessionBeanLocal() {
         try {
             javax.naming.Context c = new InitialContext();
@@ -84,7 +113,7 @@ public class EnquiryResource {
             throw new RuntimeException(ne);
         }
     }
-    
+
     private StaffSessionBeanLocal lookupStaffSessionBeanLocal() {
         try {
             javax.naming.Context c = new InitialContext();
@@ -94,5 +123,5 @@ public class EnquiryResource {
             throw new RuntimeException(ne);
         }
     }
-    
+
 }
