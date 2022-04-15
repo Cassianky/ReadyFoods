c78af068920c921b714041c3b530c26831889e89
e056 <e0560031@u.nus.edu>
2022-04-15 14:18:43 +0800

    staff resource: view all staff

--- ReadyFoodsRws/src/java/ws/rest/StaffResource.java
+++ ReadyFoodsRws/src/java/ws/rest/StaffResource.java
@@ -6,8 +6,10 @@
 package ws.rest;
 
 import ejb.session.stateless.StaffSessionBeanLocal;
+import entity.Enquiry;
 import entity.Ingredient;
 import entity.Staff;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ws.rs.core.Context;
@@ -17,9 +19,12 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 import util.exception.IngredientExistsException;
 import util.exception.IngredientNotFoundException;
 import util.exception.InputDataValidationException;
@@ -93,6 +98,56 @@ public class StaffResource {
             }
         } else {
             return Response.status(Response.Status.BAD_REQUEST).entity("Invalid create new staff request").build();
+        }
+    }
+    
+    @Path("retrieveStaff/{staffId}")
+    @GET
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response retrieveStaff(
+            @QueryParam("username") String username,
+            @QueryParam("password") String password,
+            @PathParam("staffId") Long staffId) {
+        try {
+
+            Staff currentStaff = staffSessionBeanLocal.staffLogin(username, password);
+
+            System.out.println("********** EnquiryResource.retrieveEnquiry(): Staff "
+                    + currentStaff.getUsername() + " login remotely via web service");
+
+            Staff staffToView = staffSessionBeanLocal.retrieveStaffByStaffId(staffId);
+
+            return Response.status(Status.OK).entity(staffToView).build();
+        } catch (Exception ex) {
+            return Response.status(Status.INTERNAL_SERVER_ERROR).entity(ex.getMessage()).build();
+        }
+    }
+    
+    @Path("retrieveAllStaff")
+    @GET
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response retrieveAllEnquires(@QueryParam("username") String username,
+            @QueryParam("password") String password) {
+        try {
+
+            Staff currentStaff = staffSessionBeanLocal.staffLogin(username, password);
+
+            System.out.println("********** EnquiryResource.retrieveAllStaff(): Staff "
+                    + currentStaff.getUsername() + " login remotely via web service");
+
+            List<Staff> staffs = staffSessionBeanLocal.retrieveAllStaffs();
+
+
+            GenericEntity<List<Staff>> genericEntity = new GenericEntity<List<Staff>>(staffs) {
+            };
+
+            return Response.status(Status.OK).entity(genericEntity).build();
+        } catch (InvalidLoginCredentialException ex) {
+            return Response.status(Status.UNAUTHORIZED).entity(ex.getMessage()).build();
+        } catch (Exception ex) {
+            return Response.status(Status.INTERNAL_SERVER_ERROR).entity(ex.getMessage()).build();
         }
     }
 }
