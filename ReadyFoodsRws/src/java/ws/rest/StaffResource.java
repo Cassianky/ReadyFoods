a271e3f4f77f8fd22cd070ed83547c752b8015d7
e056 <e0560031@u.nus.edu>
2022-04-16 00:34:56 +0800

    delete staff ws and sesh bean

--- ReadyFoodsRws/src/java/ws/rest/StaffResource.java
+++ ReadyFoodsRws/src/java/ws/rest/StaffResource.java
@@ -116,10 +116,11 @@ public class StaffResource {
             System.out.println("********** StaffResource.deleteStaff(): Staff "
                     + currentStaff.getUsername() + " login remotely via web service");
 
-            Staff staffToDel = staffSessionBeanLocal.retrieveStaffByStaffId(staffId);
+              
+            staffSessionBeanLocal.deleteStaff(staffId);
 
             return Response.status(Status.OK).build();
-        } catch (Exception ex) {
+        } catch (InvalidLoginCredentialException | StaffNotFoundException ex) {
             return Response.status(Status.INTERNAL_SERVER_ERROR).entity(ex.getMessage()).build();
         }
     }
