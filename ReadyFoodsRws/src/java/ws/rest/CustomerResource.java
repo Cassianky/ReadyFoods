a4e632234da0e76d5d54929935032f35c8c28881
angely <e0564603@u.nus.edu>
2022-04-15 14:00:11 +0800

    customer ws

---
+++ ReadyFoodsRws/src/java/ws/rest/CustomerResource.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ws.rest;
+
+import ejb.session.stateless.CustomerSessionBeanLocal;
+import ejb.session.stateless.StaffSessionBeanLocal;
+import entity.Customer;
+import entity.Staff;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import util.exception.InvalidLoginCredentialException;
+
+/**
+ * REST Web Service
+ *
+ * @author angler
+ */
+@Path("Customer")
+public class CustomerResource {
+
+    StaffSessionBeanLocal staffSessionBean = lookupStaffSessionBeanLocal();
+
+    CustomerSessionBeanLocal customerSessionBean = lookupCustomerSessionBeanLocal();
+    
+    
+
+    @Context
+    private UriInfo context;
+    
+    
+
+    /**
+     * Creates a new instance of CustomerResource
+     */
+    public CustomerResource() {
+    }
+    
+     @Path("retrieveAllCustomers")
+    @GET
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response retrieveAllEnquires(@QueryParam("username") String username,
+            @QueryParam("password") String password) {
+        try {
+
+            Staff staff = staffSessionBean.staffLogin(username, password);
+
+            System.out.println("********** CustomerResource.retrieveAllCustomers(): Staff "
+                    + staff.getUsername() + " login remotely via web service");
+
+            List<Customer> customers = customerSessionBean.retrieveAllCustomers();
+
+            for (Customer cust : customers) {
+                cust.getEnquiries().clear();
+                cust.getFoodDiaryRecords().clear();
+                cust.setCreditCard(null);
+                cust.getBookedmarkedRecipes().clear();
+                cust.getFoods().clear();
+                cust.getOrders().clear();
+                cust.getSubscriptions().clear();
+                cust.setSalt(null);
+                cust.setPassword(null);
+
+            }
+
+            GenericEntity<List<Customer>> genericEntity = new GenericEntity<List<Customer>>(customers) {
+            };
+
+            return Response.status(Status.OK).entity(genericEntity).build();
+        } catch (InvalidLoginCredentialException ex) {
+            return Response.status(Status.UNAUTHORIZED).entity(ex.getMessage()).build();
+        } catch (Exception ex) {
+            return Response.status(Status.INTERNAL_SERVER_ERROR).entity(ex.getMessage()).build();
+        }
+    }
+
+    private CustomerSessionBeanLocal lookupCustomerSessionBeanLocal() {
+        try {
+            javax.naming.Context c = new InitialContext();
+            return (CustomerSessionBeanLocal) c.lookup("java:global/ReadyFoods/ReadyFoods-ejb/CustomerSessionBean!ejb.session.stateless.CustomerSessionBeanLocal");
+        } catch (NamingException ne) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
+            throw new RuntimeException(ne);
+        }
+    }
+
+    private StaffSessionBeanLocal lookupStaffSessionBeanLocal() {
+        try {
+            javax.naming.Context c = new InitialContext();
+            return (StaffSessionBeanLocal) c.lookup("java:global/ReadyFoods/ReadyFoods-ejb/StaffSessionBean!ejb.session.stateless.StaffSessionBeanLocal");
+        } catch (NamingException ne) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
+            throw new RuntimeException(ne);
+        }
+    }
+
+   
+}
